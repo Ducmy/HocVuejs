fafcfc85631f45bf415c1de0a6d1643aaa34e8ca
cnvs_my <mynguyen@Macpro-my.local>
2020-05-21 23:40:09 +0700

    Data & methods

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "My~",
+    job: "IT",
+  },
+  methods: {
+    greet: function (time) {
+      return "Good " + time + " " + this.name; // Good morning My~
+    },
   },
 });
