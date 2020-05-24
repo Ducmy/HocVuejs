4b3930b1e735e9dc01af2f5fe65c93011296948f
cnvs_my <mynguyen@Macpro-my.local>
2020-05-24 13:19:42 +0700

    Key board mofify

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
 //Tạo đối tượng vue mới
 new Vue({
   el: "#vue-app",
-  data: {
-    x: 0,
-    y: 0,
-    name: "My~",
-    job: "IT",
-    website: "http://hoclamwebnhanh.com",
-    websiteTag: '<a href="http://hoclamwebnhanh.com">Hoc Lam Web Nhanh</a>',
-    age: 25,
-  },
+  data: {},
   methods: {
-    greet: function (time) {
-      return "Good " + time + " " + this.name; // Good morning My~
+    logName: function () {
+      console.log("You are enter your name");
     },
-    add: function (inc) {
-      this.age += inc;
-    },
-    sub: function (dec) {
-      this.age -= dec;
-    },
-    updateXY: function (event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-
-    click: function () {
-      alert("You are clicked me");
+    logAge: function () {
+      console.log("You are enter your age");
     },
   },
 });
