7903ad65b0901db082310be06126d0461314ed18
cnvs_my <mynguyen@Macpro-my.local>
2020-05-24 12:57:15 +0700

    Xử lý sự kiện event

--- app.js
+++ app.js
@@ -2,14 +2,27 @@
 new Vue({
   el: "#vue-app",
   data: {
+    x: 0,
+    y: 0,
     name: "My~",
     job: "IT",
     website: "http://hoclamwebnhanh.com",
     websiteTag: '<a href="http://hoclamwebnhanh.com">Hoc Lam Web Nhanh</a>',
+    age: 25,
   },
   methods: {
     greet: function (time) {
       return "Good " + time + " " + this.name; // Good morning My~
+    },
+    add: function (inc) {
+      this.age += inc;
+    },
+    sub: function (dec) {
+      this.age -= dec;
+    },
+    updateXY: function (event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     },
   },
 });
